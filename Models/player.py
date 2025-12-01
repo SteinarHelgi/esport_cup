f23698d8677964aed4292f7cfffc31778177f315
sigrunvidarsdottir <sigrunv24@ru.is>
2025-12-01 12:30:00 +0000

    THERE WAS A MERGE CONFLICT

---
+++ Models/player.py
@@ -0,0 +1,14 @@
+import datetime as dt
+   
+class Player:
+    def __init__(self, player_id: int, name: str, address: str, phone_number: int, email: str, social_media: str, team: Team, handle: str, points: int, date_of_birth: dt):
+        self.player_id = player_id
+        self.name = name
+        self.address = address
+        self.phone_number = phone_number
+        self.email = email
+        self.social_media = social_media
+        self.team = team
+        self.handle = handle
+        self.points = points
+        self.date_of_birth = date_of_birth
