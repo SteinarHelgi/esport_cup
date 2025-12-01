e27e83c7cc5ead0791e512cef88b2d3364be48ad
Þröstur Hrafnkelsson <throstur78@gmail.com>
2025-12-01 11:59:10 +0000

    ContactPerson og Tournament model klasar búnir til

---
+++ Models/ContactPerson.py
@@ -0,0 +1,12 @@
+class Tournament:
+    pass
+
+class ContactPerson:
+
+    def __init__(self, id: int, name: str, email: str, phone: str, tournament: Tournament):
+        self.id = id
+        self.name = name
+        self.email = email
+        self.phone = phone
+        self.tournament = tournament
+    
---
+++ Models/Tournament.py
@@ -0,0 +1,20 @@
+import datetime as dt
+
+class ContactPerson:
+    pass
+
+
+class Tournament:
+
+    def __init__(self, id: int, start_date: dt, end_date: dt, name: str, venue: str, games: list, no_servers: int, contact_person: ContactPerson):
+        self.id = id
+        self.start_date = start_date
+        self.end_date = end_date
+        self.name = name
+        self.venue = venue
+        self.games = games
+        self.no_servers = no_servers
+        self.contact_person_id = ContactPerson
+        
+
+ 
