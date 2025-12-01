959e371087be01ab942509e74912eeee0ede7d2a
Hilmir Ægir Ómarsson <hilmir.omars@gmail.com>
2025-12-01 11:57:59 +0000

    Organiser og TC

--- Data/organiser.py
+++ Data/organiser.py
@@ -1,2 +1,6 @@
 class Organiser():
-    
+    def __init__(self, id: int, username: str, password: str):
+        self.username = username
+        self.password = password
+        self.id = id
+
---
+++ Data/team_captain.py
@@ -0,0 +1,11 @@
+class Team():
+    def __init__(self):
+        return None
+
+class TeamCaptain():
+    def __init__(self, username: str, password: str, id: int, team: Team, name: str):
+        self.username = username
+        self.password = password
+        self.id = id
+        self.team = team
+        self.name = name
