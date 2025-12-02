7a92ee09933cc54c73fcef174736d57701d1978e
Steinar Helgi <steinarhelgi@outlook.com>
2025-12-02 17:51:46 +0000

    laga

--- IO/tournament_data.py
+++ IO/tournament_data.py
@@ -16,27 +16,28 @@ class TournamentData:
             csvReader = csv.reader(file)
             next(csvReader)  # skip header
             for line in csvReader:
-                id = int(line[0])
-                name = line[1]
-                start_date = datetime.fromisoformat(line[2])
-                end_date = datetime.fromisoformat(line[3])
-                venue = line[4]
-                game_id = line[5]
-                number_of_servers = int(line[6])
-                # contact_person_id = ContactPerson(line[7],"Hilmir","hilmir@rasshaus.is","8410536",)
-                contact_person_id = line[7]
+                if line != []:
+                    id = int(line[0])
+                    name = line[1]
+                    start_date = datetime.fromisoformat(line[2])
+                    end_date = datetime.fromisoformat(line[3])
+                    venue = line[4]
+                    game_id = line[5]
+                    number_of_servers = int(line[6])
+                    # contact_person_id = ContactPerson(line[7],"Hilmir","hilmir@rasshaus.is","8410536",)
+                    contact_person_id = line[7]
 
-                tournament = Tournament(
-                    id,
-                    name,
-                    start_date,
-                    end_date,
-                    venue,
-                    game_id,
-                    number_of_servers,
-                    contact_person_id,
-                )
-                tournaments.append(tournament)
+                    tournament = Tournament(
+                        id,
+                        name,
+                        start_date,
+                        end_date,
+                        venue,
+                        game_id,
+                        number_of_servers,
+                        contact_person_id,
+                    )
+                    tournaments.append(tournament)
         return tournaments
 
     def storeNewTournament(self, tournament: Tournament):
