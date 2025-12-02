30e75beeb19bc214a6007c8ff8e374004fb92745
Agnitto13 <agnarb97@gmail.com>
2025-12-02 15:37:39 +0000

    meira todo

---
+++ LL/team_captain_ll.py
@@ -0,0 +1,29 @@
+class TeamCaptainLL:
+    def __init__(self):
+        self.id = 0
+    
+    def create_player(self):
+        #TODO
+        pass
+
+    def modify_player(self):
+        #TODO
+        pass
+
+    def create_new_team(self):
+        #TODO
+        pass
+
+    def add_team_to_club(self):
+        #TODO
+        pass
+    
+    def modify_team_info(self):
+        #TODO
+        pass
+
+    def register_team_to_tournament(self):
+        #TODO
+        pass
+
+    
