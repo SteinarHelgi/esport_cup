2a7d377b24ce9c22b8efb531c5fde5b7d35da286
Hilmir Ægir Ómarsson <hilmir.omars@gmail.com>
2025-12-02 16:08:12 +0000

    Remove unnecessary 'self' parameter from menu functions

--- UI/list_of_menus.py
+++ UI/list_of_menus.py
@@ -4,28 +4,27 @@ def print_login_menu(): #Login menu
     print("1. continue as user \n2. Login as Team Captain \n3. Login as Organiser")
     
 
-def print_user_menu(self): #Option menu for user
+def print_user_menu(): #Option menu for user
     print("__SELECT AN OPTION__")
     print("1. Teams \n2. Tournaments \nq. Quit")
 
 
-def print_team_captain_menu(self): #Option menu for team captain
+def print_team_captain_menu(): #Option menu for team captain
     print("__SELECT AN OPTION__")
     print("1. Teams \n2. Tournaments \n3. My Team \n4. My Tournaments \nq. Quit")
 
 
-def print_organiser_menu(self):#option menu for organiser
+def print_organiser_menu():#option menu for organiser
     print("__SELECT AN OPTION__")
     print("1. Teams \n2. Tournaments \n3. Create Tournaments \n4. My Tournaments \nq. Quit")
 
-def print_team_menu(self): #Prints a list of teams with the corresponding information
+def print_team_menu(): #Prints a list of teams with the corresponding information
     list_of_teams = 0
     return list_of_teams
     #TODO add options for this menu
 
-def print_tournament_menu(self): #Menu for looking at tournaments
+def print_tournament_menu(): #Menu for looking at tournaments
     print("1. Ongoing Tournaments \n2. Upcoming Tournaments \n3. Past Tournaments \nb. Back \nq.Quit")
-    
     
     
 def print_my_team_menu(): #Options for my team
@@ -36,19 +35,14 @@ def print_my_team_menu(): #Options for my team
     captain = 0
     print (f"{club} {team_name} {link} {logo} {captain}")
     print ("1. Add team to club \n2. Roster \n3. Edit team info \nb. Back \nq. Quit")
-    
-    
-    
-def tournaments_for_my_team(self):
+
+
+def tournaments_for_my_team():
     tournaments_registered = 0
     return tournaments_registered
 
-def register_for_tournament(self):
+
+def register_for_tournament():
     available_tournaments = 0
     print(f"{available_tournaments}")
-
-
-            
-    
-
 
