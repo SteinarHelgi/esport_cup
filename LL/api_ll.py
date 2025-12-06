490539d312986ea7e9367f55cb4e7f8ac9fc50a9
sigrunvidarsdottir <sigrunv24@ru.is>
2025-12-06 15:50:39 +0000

    api_ll

--- LL/api_ll.py
+++ LL/api_ll.py
@@ -16,64 +16,72 @@ class APILL:
 
     def get_all_teams(self) -> list[Team]:
         return self.userLL.get_all_teams()
-
-    def get_players_in_team(self, team: str) -> list[Player]:
-        return self.team_captain_ll.get_players_in_team(team)
-
+    
     def get_all_tournaments(self) -> list[Tournament]:
         return self.api_data.get_all_tournament_data()
-
-    def get_tournament_by_name(self, tournament_name: str) -> Tournament | None:
-        return self.organiser_ll.get_tournament_by_name(tournament_name)
-
+    
+    def get_all_players(self) -> list[Player]:
+        return self.userLL.get_all_players()
+    
     def get_ongoing_tournaments(self) -> list[Tournament]:
         return self.userLL.get_ongoing_tournament()
-
+    
     def get_past_tournaments(self) -> list[Tournament]:
         return self.userLL.get_past_tournament()
-
+    
     def get_upcoming_tournaments(self) -> list[Tournament]:
         return self.userLL.get_upcoming_tournament()
 
+    def create_player(self, player: Player) -> Player | None:
+        return self.team_captain_ll.create_player(player)
+    
+    def create_new_team(self, team: Team) -> Team | None:
+        return self.team_captain_ll.create_new_team(team)
+    
+    def register_team_to_tournament(self, team: Team, tournament: Tournament):
+        return self.team_captain_ll.register_team_to_tournament(team, tournament)
+    
     def get_team_by_captain_id(self, id) -> Team | None:
         return self.team_captain_ll.get_team_by_captain_id(id)
-
+    
+    def get_players_in_team(self, team: str) -> list[Player]:
+        return self.team_captain_ll.get_players_in_team(team)
+    
     def get_team_by_name(self, name) -> Team | None:
         return self.team_captain_ll.get_team_by_name(name)
+    
+    def get_player_by_name(self, player_name: str) -> Player | None:
+        return self.team_captain_ll.get_player_by_name(player_name)
+    
+    def show_all_tournaments_for_captain(self, captain: TeamCaptain) -> TeamCaptain:
+        return self.team_captain_ll.show_all_tournaments_for_captain(captain)
+    
+    def show_all_open_tournaments_for_captain(self,captain: TeamCaptain) -> TeamCaptain:
+        return self.team_captain_ll.show_all_open_tournaments_for_captain(captain)
 
     def create_tournament(self, tournament: Tournament) -> Tournament | None:
         return self.organiser_ll.create_tournament(tournament)
 
+    def get_tournament_by_name(self, tournament_name: str) -> Tournament | None:
+        return self.organiser_ll.get_tournament_by_name(tournament_name)
+    
+    def delete_tournament(self, tournament_id: str):
+        return self.organiser_ll.delete_tournament(tournament_id)
+    
     def create_match(self, match: Match) -> Match | None:
         return self.organiser_ll.create_match(match)
 
     def create_contact_person(self, contact_person: ContactPerson) -> ContactPerson | None:
         return self.organiser_ll.create_contact_person(contact_person)
-
-    def get_player_by_name(self, player_name: str) -> Player | None:
-        return self.team_captain_ll.get_player_by_name(player_name)
     
-    def create_new_team(self, team: Team) -> Team | None:
-        return self.team_captain_ll.create_new_team(team)
+    def get_contact_person_by_id(self, id: str) -> ContactPerson | None:
+        return self.organiser_ll.get_contact_person_by_id(id)
     
-    def create_player(self, player: Player) -> Player | None:
-        return self.team_captain_ll.create_player(player)
-
-    def register_team_to_tournament(self, team: Team, tournament: Tournament):
-        return self.team_captain_ll.register_team_to_tournament(team, tournament)
+    def get_contact_person(self, tournament_id: str) -> ContactPerson | None:
+        return self.organiser_ll.get_contact_person(tournament_id)
     
     def show_all_teams_on_tournament(self, target_tournament_id: str) -> list[Team]:
         return self.organiser_ll.show_all_teams_on_tournament(target_tournament_id)
     
-    def show_all_tournaments_for_captain(self, captain: TeamCaptain) -> TeamCaptain:
-        return self.team_captain_ll.show_all_tournaments_for_captain(captain)
-    
-    def delete_tournament(self, tournament_id: str):
-        return self.organiser_ll.delete_tournament(tournament_id)
-    
-    def show_all_open_tournaments_for_captain(self,captain: TeamCaptain) -> TeamCaptain:
-        return self.team_captain_ll.show_all_open_tournaments_for_captain(captain)
-   
-    def get_all_players(self) -> list[Player]:
-        return self.userLL.get_all_players()
-
+    def register_match_result(self, match_id: str, home_score: int, away_score: int, completed: bool):
+        return self.organiser_ll.register_match_result(match_id, home_score, away_score, completed)    
