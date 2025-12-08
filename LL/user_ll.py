4bf58dda87bb765075946722ba7cd780a3ce3b8b
Steinar Helgi <steinarhelgi@outlook.com>
2025-12-08 11:31:20 +0000

    commenta út stats

--- LL/user_ll.py
+++ LL/user_ll.py
@@ -4,11 +4,13 @@ from Models.team import Team
 from Models.tournament import Tournament
 from Models.match import Match
 from Models.player import Player
-from Models.club_stat import ClubStat
-from Models.elimination_stat import EliminationStat
-from Models.player_stat import PlayerStat
-import matplotlib.pyplot as plt
+
+# from Models.club_stat import ClubStat
+# from Models.elimination_stat import EliminationStat
+# from Models.player_stat import PlayerStat
+# import matplotlib.pyplot as plt
 from typing import List, Tuple
+
 
 class UserLL:
     def __init__(self, api_data: APIDATA):
@@ -70,101 +72,101 @@ class UserLL:
 
         return upcoming
 
-    def show_player_stats_bar_chart(self):
-        """Retrieves player points and shows a bar chart of the top players"""
-        #Breyta stats yfir í dict
-        players = self.api_data.get_all_player_data()
-        stats: dict = {}
-        #stats: list[list[Union[int, str]]] = []
-        for player in players:
-            stats[player.handle] = player.points
-        #Sort the results by points - highest is first.
-        sorted_items: List[Tuple[str, int]] = sorted(stats.items(), key=lambda item: item[1])
-
-        #Going to show this many players in our bar chart
-        number_of_players_to_display = 5
-        
-        #creating the value and keys for the bar chart 
-        handle: list[str] = []
-        points: list[int] = []
-        for i in range(number_of_players_to_display):
-            handle.append(sorted_items[i][1])
-            points.append(sorted_items[i][0])
-        plt.bar(handle, points)
-        plt.show()
-        
-        
-
-    def get_elimination_stats(self, tournament_id):
-        matches = self.api_data.get_all_match_data()
-        team_registry = self.api_data.get_all_team_registry_data()
-        teams = self.api_data.get_all_team_data()
-        stats: list[EliminationStat] = []
-        for registry in team_registry:
-            if registry.tournament_id == tournament_id:
-                stat = EliminationStat(registry.team_name)
-                for match in matches:
-                    if match.completed == "TRUE":
-                        if match.team_a_name == stat.team_name:
-                            stat.games_played += 1
-                            stat.score_for += int(match.score_a)
-                            stat.score_against += int(match.score_b)
-                        if match.team_b_name == stat.team_name:
-                            stat.games_played += 1
-                            stat.score_for += int(match.score_b)
-                            stat.score_against += int(match.score_a)
-                        if match.winner_team_name == stat.team_name:
-                            stat.won += 1
-                stat.lost = stat.games_played - stat.won
-                if stat.games_played == 0:
-                    stat.winning_ratio = 0
-                else:
-                    stat.winning_ratio = stat.won/stat.games_played * 100
-                for team in teams:
-                    if team.name == registry.team_name:
-                        stat.points = team.points
-                        break
-                stats.append(stat)
-            
-        sorted_by_winning_ratio = sorted(stats, key=lambda s: s.winning_ratio, reverse = True)
-        
-        return sorted_by_winning_ratio
-
-    def get_all_player_stat(self, tournament_id) -> list[PlayerStat]:
-        all_player_stats: list[PlayerStat] = []
-        matches: list[Match] = self.api_data.get_all_match_data()
-        players: list[Player] = self.api_data.get_all_player_data()
-        elimination_stat: list[EliminationStat] = self.get_elimination_stats(tournament_id)
-        for player in players:
-            team_name = player.team_name
-            player_stat: PlayerStat = PlayerStat(player.handle)
-            player_stat.points = player.points
-            for stat in elimination_stat:
-                if stat.team_name == team_name:
-                    player_stat.games_played = int(stat.games_played)
-                    player_stat.won = int(stat.won)
-                    player_stat.lost = int(stat.lost)
-                    player_stat.winning_ratio = float(stat.winning_ratio)
-            all_player_stats.append(player_stat)
-        return all_player_stats
-    
-    def get_player_stat(self, player_handle, tournament_id) -> PlayerStat | None:
-        all_player_stat = self.get_all_player_stat(tournament_id)
-        for player_stat in all_player_stat:
-            if player_stat.player_handle == player_handle:
-                return player_stat
-        return None
-    
-    def get_all_club_stat(self)->list[ClubStat]:
-        all_clubs_stat: list[ClubStat] = []
-        #clubs: list[Club] = self.get_all_club_data()
-        #eliminations_stat = #TODO Sækja hér elimination data úr organiser_ll klasanum.
-        for club in clubs:
-            new_club = ClubStat(club.name)
-            #teams_in_club = self.get_all_teams_in_a_club(club.id)
-            #for team in teams_in_club:
-             #   pass
-
-
-
-        return all_clubs_stat
+    # def show_player_stats_bar_chart(self):
+    #     """Retrieves player points and shows a bar chart of the top players"""
+    #     #Breyta stats yfir í dict
+    #     players = self.api_data.get_all_player_data()
+    #     stats: dict = {}
+    #     #stats: list[list[Union[int, str]]] = []
+    #     for player in players:
+    #         stats[player.handle] = player.points
+    #     #Sort the results by points - highest is first.
+    #     sorted_items: List[Tuple[str, int]] = sorted(stats.items(), key=lambda item: item[1])
+    #
+    #     #Going to show this many players in our bar chart
+    #     number_of_players_to_display = 5
+    #
+    #     #creating the value and keys for the bar chart
+    #     handle: list[str] = []
+    #     points: list[int] = []
+    #     for i in range(number_of_players_to_display):
+    #         handle.append(sorted_items[i][1])
+    #         points.append(sorted_items[i][0])
+    #     plt.bar(handle, points)
+    #     plt.show()
+    #
+    #
+    #
+    # def get_elimination_stats(self, tournament_id):
+    #     matches = self.api_data.get_all_match_data()
+    #     team_registry = self.api_data.get_all_team_registry_data()
+    #     teams = self.api_data.get_all_team_data()
+    #     stats: list[EliminationStat] = []
+    #     for registry in team_registry:
+    #         if registry.tournament_id == tournament_id:
+    #             stat = EliminationStat(registry.team_name)
+    #             for match in matches:
+    #                 if match.completed == "TRUE":
+    #                     if match.team_a_name == stat.team_name:
+    #                         stat.games_played += 1
+    #                         stat.score_for += int(match.score_a)
+    #                         stat.score_against += int(match.score_b)
+    #                     if match.team_b_name == stat.team_name:
+    #                         stat.games_played += 1
+    #                         stat.score_for += int(match.score_b)
+    #                         stat.score_against += int(match.score_a)
+    #                     if match.winner_team_name == stat.team_name:
+    #                         stat.won += 1
+    #             stat.lost = stat.games_played - stat.won
+    #             if stat.games_played == 0:
+    #                 stat.winning_ratio = 0
+    #             else:
+    #                 stat.winning_ratio = stat.won/stat.games_played * 100
+    #             for team in teams:
+    #                 if team.name == registry.team_name:
+    #                     stat.points = team.points
+    #                     break
+    #             stats.append(stat)
+    #
+    #     sorted_by_winning_ratio = sorted(stats, key=lambda s: s.winning_ratio, reverse = True)
+    #
+    #     return sorted_by_winning_ratio
+    #
+    # def get_all_player_stat(self, tournament_id) -> list[PlayerStat]:
+    #     all_player_stats: list[PlayerStat] = []
+    #     matches: list[Match] = self.api_data.get_all_match_data()
+    #     players: list[Player] = self.api_data.get_all_player_data()
+    #     elimination_stat: list[EliminationStat] = self.get_elimination_stats(tournament_id)
+    #     for player in players:
+    #         team_name = player.team_name
+    #         player_stat: PlayerStat = PlayerStat(player.handle)
+    #         player_stat.points = player.points
+    #         for stat in elimination_stat:
+    #             if stat.team_name == team_name:
+    #                 player_stat.games_played = int(stat.games_played)
+    #                 player_stat.won = int(stat.won)
+    #                 player_stat.lost = int(stat.lost)
+    #                 player_stat.winning_ratio = float(stat.winning_ratio)
+    #         all_player_stats.append(player_stat)
+    #     return all_player_stats
+    #
+    # def get_player_stat(self, player_handle, tournament_id) -> PlayerStat | None:
+    #     all_player_stat = self.get_all_player_stat(tournament_id)
+    #     for player_stat in all_player_stat:
+    #         if player_stat.player_handle == player_handle:
+    #             return player_stat
+    #     return None
+    #
+    # def get_all_club_stat(self)->list[ClubStat]:
+    #     all_clubs_stat: list[ClubStat] = []
+    #     #clubs: list[Club] = self.get_all_club_data()
+    #     #eliminations_stat = #TODO Sækja hér elimination data úr organiser_ll klasanum.
+    #     for club in clubs:
+    #         new_club = ClubStat(club.name)
+    #         #teams_in_club = self.get_all_teams_in_a_club(club.id)
+    #         #for team in teams_in_club:
+    #          #   pass
+    #
+    #
+    #
+    #     return all_clubs_stat
