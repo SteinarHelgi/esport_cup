e82512724f3f5aba5aff2eddbc9d14b60e116406
Hilmir Ægir Ómarsson <hilmir.omars@gmail.com>
2025-12-11 21:02:37 +0000

    shrek is love shrek is life

--- UI/shrek.py
+++ UI/shrek.py
@@ -70,5 +70,18 @@ def shrek():
                                                   ;;...;.                                                          .....;;
                                                                            .;                                      ....;.;    
     """)
+    print("""I was only nine years old. I loved shrek so much. I had all the merchandise and movies
+          I'd pray to shrek every night before i go to bed, thanking for the life i've been given.
+          'Shrek is love', i would say,'Shrek is life'. My dad hears me and calls me a faggot.
+          i knew he was just jealous for my devotion of Shrek. I called him a cunt. He slaps me and sends
+          me to go to sleep. I'm crying now and my face hurts. I lay in bed and it's really cold.
+          A warmth is moving towards me. I feel something touch me. It's Shrek. I'm so happy. 
+          He whispers in my ear,"This is my swamp". He grabs me with his powerful ogre hands, and puts
+          me on my hands and knees. I spread my ass-cheeks for Shrek. He penetrates my butthole.
+          It hurts so much, but i do it for Shrek. I can feel my butt tearing as my eyes start to water.
+          I push against his force. I want to please Shrek. He roars a mighty roar, as he fills my butt 
+          with his love. My dad walks in. Shrek looks him straigt in the eye, and says,
+          "Its all ogre now". Shrek leaves through my window. Shrek is love. Shrek is life.
+          """)
     leave = input("Enter to leave shrek behind")
     return "LOGIN_MENU"
