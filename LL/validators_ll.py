afad82eb5d8265d267fa391d7f8f7d42b4df6181
Steinar Helgi <steinarhelgi@outlook.com>
2025-12-11 11:28:08 +0000

    VALIDATION FYRIR MODIFY PLAYER

--- LL/validators_ll.py
+++ LL/validators_ll.py
@@ -139,7 +139,7 @@ def validate_social_media(social_media):
     socials = social_media.strip()
     if socials == "":
         return Errors.EMPTY
-    if socials == " ":
+    if " " in socials:
         return Errors.HANDLE_CONTAINS_SPACE
     return Errors.OK
 
